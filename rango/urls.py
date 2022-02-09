6ef8838e4c855bdc914bcb2570fb09520079d900
Stuart McAdam <2094313m@student.gla.ac.uk>
2022-02-09 17:20:16 +0000

    chapter 7 tests passing!

--- rango/urls.py
+++ rango/urls.py
@@ -8,9 +8,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('category/<slug:category_name_slug>/',
          views.show_category, name='show_category'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('add_category/', views.add_category, name='add_category'),
+    path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
 ]
