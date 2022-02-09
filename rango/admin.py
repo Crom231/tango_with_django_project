9d4f4eb961f563988fb703052dffa4ad9b716799
Stuart McAdam <2094313m@student.gla.ac.uk>
2022-02-09 15:42:13 +0000

    chapter 5 test passing (1st Try! WooYeah)

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
