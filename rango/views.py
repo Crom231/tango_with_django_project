571b5e8479b062192c87637c1b7533ac5f8f0de3
Stuart McAdam <2094313m@student.gla.ac.uk>
2022-02-10 10:00:23 +0000

    chapter 9.9

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,9 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
-from rango.models import Category, Page, UserProfile
+from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 
@@ -214,8 +214,17 @@ def user_login(request):
         # blank dictionary object...
         return render(request, 'rango/login.html')
 
+
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
 
 
+# Use the login_required() decorator to ensure only those logged in can
+# access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # Take the user back to the homepage.
+    return redirect(reverse('rango:index'))
