a9ca015e2a7f924ccea73af9356a5266a492f208
Stuart McAdam <2094313m@student.gla.ac.uk>
2022-02-10 09:57:28 +0000

    chapter 9.8

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
@@ -212,4 +213,9 @@ def user_login(request):
         # No context variables to pass to the template system, hence the
         # blank dictionary object...
         return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
 
