0d56d8b3034fb49d9df19bf9f494e11ee891bbc6
Stuart McAdam <2094313m@student.gla.ac.uk>
2022-02-10 10:15:11 +0000

    chapter 9 tests passing!

--- rango/views.py
+++ rango/views.py
@@ -60,6 +60,7 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context=context_dict)
 
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     # A HTTP POST?
@@ -81,6 +82,7 @@ def add_category(request):
     return render(request, 'rango/add_category.html', {'form': form})
 
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -217,7 +219,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 
 # Use the login_required() decorator to ensure only those logged in can
