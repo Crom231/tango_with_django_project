6114d835c137cf90485c55a2b9f38cf06774ddfb
Stuart McAdam <2094313m@student.gla.ac.uk>
2022-02-10 12:06:59 +0000

    chapter 10.4

--- rango/views.py
+++ rango/views.py
@@ -19,11 +19,15 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+    request.session.set_test_cookie()
     # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     context_dict = {'boldmessage': 'This tutorial has been put together by Stuart McAdam'}
     return render(request, 'rango/about.html', context=context_dict)
 
